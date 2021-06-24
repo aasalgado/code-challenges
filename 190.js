8d58eb9a4405aa2aa47f6ccd8a5e6291ba040bd6
AnthonyS <anthony.salgado@omnichains.com>
2021-06-24 16:31:44 -0700

    190 done

--- 190.js
+++ 190.js
@@ -1,3 +1,9 @@
+// #190 Reverse Bits
+// n = 00000010100101000001111010011100
 var reverseBits = function(n) {
-    
+    const reverseStr = n.toString(2).split('').reverse();
+    while(reverseStr.length <32){ reverseStr.push('0')}
+    return parseInt(reverseStr.join(''), 2);
 };
+
+console.log(reverseBits(00000010100101000001111010011100))
