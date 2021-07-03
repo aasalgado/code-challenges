67451b0399bc5c66d16766dbb709a4b31e8bb2ef
Anthony Salgado <anthonyasalgado@gmail.com>
2021-07-03 15:29:42 -0700

    save

--- June/wk4/219.js
+++ June/wk4/219.js
@@ -14,6 +14,6 @@ var containsNearbyDuplicate = function(nums, k) {
    return false;
 };
 
-console.log(containsNearbyDuplicate([1,2,3,1], 3))
-console.log(containsNearbyDuplicate([1,0,1,1], 1))
-console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2))
+console.log(containsNearbyDuplicate([1,2,3,1], 3));
+console.log(containsNearbyDuplicate([1,0,1,1], 1));
+console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2));
