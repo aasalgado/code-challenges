b77c91b7869041bb07316113445d329e87af552e
Anthony Salgado <anthonyasalgado@gmail.com>
2021-07-05 22:34:17 -0700

    225

---
+++ June/wk4/225.js
@@ -0,0 +1,21 @@
+// 225. Implement Stack using Queues
+
+var MyStack = function() {
+    this.stack = [];
+};
+
+MyStack.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+MyStack.prototype.pop = function() {
+    return this.stack.pop();
+};
+
+MyStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+MyStack.prototype.empty = function() {
+    return this.stack.length === 0;
+};
