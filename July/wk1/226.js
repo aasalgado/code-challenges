b9064c09eebe84ae9dadf89eaef699f1c577d0e9
Anthony Salgado <anthonyasalgado@gmail.com>
2021-07-05 23:22:18 -0700

    226

---
+++ July/wk1/226.js
@@ -0,0 +1,10 @@
+// 226. Invert Binary Tree
+var invertTree = function(root) {
+    if(!root) return null;
+
+    const left = invertTree(root.left);
+    const right = invertTree(root.right);
+    root.left = right;
+    root.right = left;
+    return root;
+};
