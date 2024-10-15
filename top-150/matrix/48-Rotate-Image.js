827853df94d3f86f20e5933b8979e2a004e2aa08
Anthony Salgado <anthonyasalgado@gmail.com>
2024-10-15 11:52:28 -0700

    48 rotate image

---
+++ top-150/matrix/48-Rotate-Image.js
@@ -0,0 +1,13 @@
+function rotate(matrix) {
+    // Step 1: Transpose the matrix (swap rows and columns)
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix[0].length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    
+    // Step 2: Reverse each row
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i].reverse();
+    }
+}
