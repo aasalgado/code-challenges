7cb88649ff56ae118ec089be034f084eca02e559
Anthony Salgado <anthonyasalgado@gmail.com>
2025-01-25 14:50:56 -0800

    3sum

---
+++ Array/15-3sum.js
@@ -0,0 +1,29 @@
+var threeSum = function (nums) {
+  let res = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicates
+
+    let left = i + 1,
+      right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        while (nums[left] === nums[left + 1]) left++; // Skip duplicates
+        while (nums[right] === nums[right - 1]) right--; // Skip duplicates
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+};
